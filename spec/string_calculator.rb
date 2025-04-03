54b9d15faf6a6cb82938be7b4d2424e4130d0aff
gayathritalasila <gayathritalasila36@gmail.com>
2025-04-03 21:42:10 +0530

    test: add method supports newline as delimiter

--- spec/string_calculator.rb
+++ spec/string_calculator.rb
@@ -16,5 +16,10 @@ RSpec.describe StringCalculator do
         expect(calculator.add("1,2")).to eq(3)
         expect(calculator.add("4,5")).to eq(9)
     end
+    it "handles new lines as delimiters" do
+      calculator = StringCalculator.new
+      expect(calculator.add("1\n2,3")).to eq(6)
+      expect(calculator.add("4\n5,6")).to eq(15)
+    end
   end
 end
